c989a2289fc9f671b89fc11fb2c20f2938007470
Shubham6822 <sharmashubham6822@gmail.com>
2024-12-25 10:23:29 +0530

    test solidity

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
+
 import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFTMarket is ERC721URIStorage {
-    uint256 private _ids =0;
+    uint256 private _ids = 0;
+
+    // Declare an event for NFT creation
+    event NFTCreated(address indexed creator, uint256 tokenId, string tokenURI);
 
     constructor() ERC721("NFTMarket", "NFTM") {}
 
@@ -11,6 +15,10 @@ contract NFTMarket is ERC721URIStorage {
         _ids++;
         _mint(msg.sender, _ids);
         _setTokenURI(_ids, tokenURL);
+
+        // Emit the NFTCreated event
+        emit NFTCreated(msg.sender, _ids, tokenURL);
+
         return _ids;
     }
-}   
+}
